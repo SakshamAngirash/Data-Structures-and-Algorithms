6a633ba1be1bc3494759700311dc2db133a82e2d
Saksham <sakshamsharma3161@gmail.com>
2023-09-29 00:09:08 +0530

    29sept

--- Week5/Vector/reversecopy.cpp
+++ Week5/Vector/reversecopy.cpp
@@ -13,4 +13,17 @@ int main() {
         cin >> x;
         v.push_back(x);
     }
+    //copy array
+    vector<int> v2;
+    for (int i = 0; i < n; i++)
+    {
+        int x = v[n-i-1];
+        v2.push_back(x);
+    }
+    cout<<"Reversed array : ";
+    for (int i = 0; i < n; i++) {
+        cout<<v2.at(i)<<" ";
+    }
+
+
 }
